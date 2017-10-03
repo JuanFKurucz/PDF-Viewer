7b66ac95ed3e247f1b80a8902a3d1aedd017cb69
JuanFKurucz <31422367+JuanFKurucz@users.noreply.github.com>
2017-10-03 20:20:24 -0300

    A summary per pdf

--- window/View/js/userNotes.js
+++ window/View/js/userNotes.js
@@ -1,11 +1,23 @@
 var fs = require('fs');
 
+function pdfActive(){
+  var contenedor=document.getElementsByClassName("col-sm-8")[0].getElementsByTagName("webview");
+  for(var w=0;w<contenedor.length;w++){
+    if(contenedor[w].style.display!="none"){
+      return contenedor[w].getAttribute("pdf").split(".")[0];
+    }
+  }
+  return false;
+}
 
 exports.userNotes= (appdata)=>{
   var resumen = document.getElementById('userNotes').value;
+  var nombrePDF=pdfActive();
   console.log(document.getElementById('userNotes').value);
-  fs.writeFile(appdata+'resumen.txt', resumen, (err)=>{
-    if(err) throw err;
-    console.log('Your notes has been saved');
-  })
+  if(nombrePDF != false){
+    fs.writeFile(appdata+nombrePDF+'.txt', resumen, (err)=>{
+      if(err) throw err;
+      console.log('Your notes has been saved');
+    })
+  }
 }
