2cd5530238071b59d1ce76468ecfc28d7a37d291
JuanFKurucz <31422367+JuanFKurucz@users.noreply.github.com>
2017-10-03 19:51:52 -0300

    Boom!

--- window/View/js/script.js
+++ window/View/js/script.js
@@ -1,29 +1,15 @@
 const {ipcRenderer} = require('electron');
+const {addNewPdfItem} = require(__dirname+"/js/menu.js");
 var appData;
-
-function addNewPdfItem(name){
-  var newLi = document.createElement("li");
-  newLi.textContent=name;
-  newLi.onclick=function(){
-    document.getElementById("pdf").src=appData+name;
-  }
-  newLi.style="cursor:pointer;"
-  document.getElementById("pdfList").append(newLi);
-}
 
 function init(){
   ipcRenderer.on('path', (event, arg) => {
     appData=arg+"/Library/";
-    document.getElementById("pdf").src=appData+"pdf.pdf";
   })
   ipcRenderer.send('loaded')
-  window.addEventListener('resize', function(event) {
-    document.getElementById("pdf").style.height=window.innerHeight+"px";
-  });
   ipcRenderer.on('addPdfItems', (event, list) => {
-    console.log(list);
     for(element in list){
-      addNewPdfItem(list[element]);
+      addNewPdfItem(appData,list[element]);
     }
   })
 
