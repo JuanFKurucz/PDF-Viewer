adba0a8cb8d30a1ba141ffe5ca2ec2bf20061911
JFK PC <juanfkurucz@gmail.com>
2017-10-09 00:06:05 -0200

    Search Box and functions added

--- window/View/js/script.js
+++ window/View/js/script.js
@@ -3,6 +3,7 @@ const {addNewPdfItem} = require(__dirname+"/js/menu.js");
 const {userNotes} = require(`${__dirname}/js/userNotes.js`);
 const addNewPdf = require(`${__dirname}/js/addNewPdf.js`);
 const exportJsonSummary = require(`${__dirname}/js/exportJsonSummary.js`);
+const {searchPDF} = require(`${__dirname}/js/search.js`);
 const jQuery = require('jQuery');
 const Popper = require('popper.js');
 require('bootstrap');
@@ -22,6 +23,14 @@ function init(){
     userNotes(appData,this.getAttribute("pdfActive"));
     document.getElementById("notes").value="";
   }
-
+  document.querySelector("#search").onchange=function(){
+    searchPDF(this.value);
+  }
+  document.querySelector("#search").onkeydown=function(){
+    searchPDF(this.value);
+  }
+  document.querySelector("#search").onkeyup=function(){
+    searchPDF(this.value);
+  }
   ipcRenderer.send('askPdfList')
 }
---
+++ window/View/js/search.js
@@ -0,0 +1,17 @@
+
+exports.searchPDF=function(name){
+  var contenedor=document.getElementById("pdfList").getElementsByTagName("li");
+  if(name==""){
+    for(var i=0;i<contenedor.length;i++){
+      contenedor[i].style.display="";
+    }
+  } else {
+    for(var i=0;i<contenedor.length;i++){
+      if(contenedor[i].textContent.toLowerCase().includes(name.toLowerCase())){
+        contenedor[i].style.display="";
+      } else {
+        contenedor[i].style.display="none";
+      }
+    }
+  }
+}
